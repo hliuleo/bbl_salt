8ffdcc608c1f9b19826bcdb85e739e5ba8d960b3
hliu <antiantifido@gmail.com>
2016-12-26 23:21:26 +0800

    fix bug of cs

--- cs.py
+++ cs.py
@@ -11,26 +11,29 @@ from matplotlib.ticker import FuncFormatter
 from plot_set import *
 import os
 
-os.chdir('/home/hliu/Desktop/BBL_Salt_MakeUp/analysis/chemical_shift')
+os.chdir('/Users/hliu/Downloads/BBL_Salt_MakeUp/analysis/chemical_shift')
 
-seq = np.loadtxt('sequence_3L.dat', dtype=str)
+seq_3L = np.loadtxt('sequence_3L.dat', dtype=str)
+seq_L = np.loadtxt('sequence.dat', dtype=str)
+seqs = {'1L': seq_L, '3L': seq_3L}
+num_res = len(seq_L)
 cs_native = sio.loadmat('native_chemicalShift.mat')
 cs_random = np.loadtxt('cs_randomCoil.dat')
-selected_atomTypes = ['H', 'N']
+
 
 class ChemicalShift:
 
-    def __init__(self, trjname, seq):
+    def __init__(self, trjname, seqs):
         self.name = trjname
-        self.seq = pd.Series(seq,
-                            index=np.arange(1, len(seq)+1))
+        self.seq = pd.DataFrame(seqs, 
+                                index=np.arange(1, num_res+1))
 
     def loadShift(self, shiftData):
         atom_types = ['H', 'HA', 'N', 'C', 'CA', 'CB']
-        shift_values = {t: shiftData[t].mean(axis=0) for t in atom_types}
-        shift_values.update({t+'_E': shiftData[t].std(axis=0) for t in atom_types})
+        shift_values = {t: np.nanmean(shiftData[t], axis=0) for t in atom_types}
+        shift_values.update({t+'_E': np.nanstd(shiftData[t], axis=0) for t in atom_types})
         shift_values = pd.DataFrame(shift_values)
-        shift_values.index = np.arange(1, len(seq)+1)
+        shift_values.index = np.arange(1, num_res+1)
         shift_values.index.name = 'ResID'
         self.shift = shift_values
 
@@ -40,46 +43,73 @@ class ChemicalShift:
     def setPlot(self, getPlot):
         self.plot = getPlot(self.coor)
 
-plotted_index = np.delete(np.arange(1, 46), 7)
+plotted_idx = np.delete(np.arange(1, 46), 7)
 getPlot = lambda x: x.ix[plotted_idx, :]
 
 def setData(trjName, fPth):
-    cs = ChemicalShift(trjName, seq)
+    cs = ChemicalShift(trjName, seqs)
     cs.loadShift(sio.loadmat(fPth))
     cs.setCoor(selected_atomTypes)
     cs.setPlot(getPlot)
     return cs
-s
+
+selected_atomTypes = ['H', 'N']
 cs_6 = setData('6M low', '6M/chemicalShift.mat')
 cs_4 = setData('4M low', '4M/chemicalShift.mat')
 cs_native = setData('Native', 'native_chemicalShift.mat')
 
 
 comp1 = cs_4
-comp2 = cs_native
-plt.scatter(comp1.plot['H'], comp1.plot['N'], c='r')
-plt.scatter(comp2.plot['H'], comp2.plot['N'], c='b')
+comp2 = cs_6
+plt.scatter(comp1.plot['H'], comp1.plot['N'],
+            c='b', alpha=0.7, label=comp1.name, s=100)
+plt.scatter(comp2.plot['H'], comp2.plot['N'],
+            c='r', alpha=0.7, label=comp2.name, s=100)
+
+ax = plt.gca()
+ax.invert_yaxis()
+ax.invert_xaxis()
+
+plt.legend(frameon=False, loc=2)
+
+for idx in comp1.coor.index:
+    v1 = comp1.coor.ix[idx].values
+    v2 = comp2.coor.ix[idx].values
+    d = np.linalg.norm(v1-v2)
+    if d > 3.5:
+        plt.plot([v1[0], v2[0]],
+                 [v1[1], v2[1]],
+                 c='k', alpha=0.5)
+        plt.text(v2[0], v2[1], comp1.seq['1L'][idx]+str(idx-4), fontsize=20)
+
+plt.xlabel(r'$^{1}$H Chemical Shift')
+plt.ylabel(r'$^{15}$N Chemical Shift')
 
 for idx in plotted_idx:
     diff = np.linalg.norm(comp2.coor.loc[idx, :].values-comp1.coor.loc[idx, :].values)
-    print diff, idx, comp1.seq[idx]
+    print diff, idx, comp1.seq['3L'][idx]
 
 width = 0.3
-comp1 = cs_4_coor
-comp2 = cs_6_coor
 
-plt.bar(comp1.index-width,
-        comp1['HA'].values-cs_random,
+selected_atomTypes = ['HA', 'HA_E']
+cs_6.setCoor(selected_atomTypes)
+cs_4.setCoor(selected_atomTypes)
+cs_native.setCoor(selected_atomTypes)
+comp1 = cs_4
+comp2 = cs_6
+
+plt.bar(comp1.coor.index-width,
+        comp1.coor['HA'].values-cs_random,
         width=width,
         label=comp1.name)
-plt.errorbar(comp1.index-width/2,
-             comp1['HA'].values-cs_random,
-             yerr=comp1['HA_E'].values,
+plt.errorbar(comp1.coor.index-width/2,
+             comp1.coor['HA'].values-cs_random,
+             yerr=comp1.coor['HA_E'].values,
              ecolor='gray',
              elinewidth=1.5,
              linewidth=0)
-plt.bar(comp2.index,
-        comp2['HA'].values-cs_random,
+plt.bar(comp2.coor.index,
+        comp2.coor['HA'].values-cs_random,
         width=width,
         color='r',
         label=comp2.name)
@@ -88,4 +118,6 @@ set_tickinterval(ax, 'x', 5)
 plt.legend(loc=2, frameon=False)
 plt.grid()
 plt.xlim([-1,46])
+plt.xlabel('Residue ID')
+plt.ylabel('Secondary Shift')
 ax.xaxis.set_major_formatter(FuncFormatter(lambda x, i: str(int(x))))
